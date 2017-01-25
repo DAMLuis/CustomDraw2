e608da9433506c571a16bd8a4a9f59c584ff6476
DAMLuis <luismdam@gmail.com>
2017-01-25 13:33:11 +0100

    v1

--- app/src/main/java/com/example/luis/customview2/MainActivity.java
+++ app/src/main/java/com/example/luis/customview2/MainActivity.java
@@ -1,18 +1,7 @@
 package com.example.luis.customview2;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import com.example.luis.customview2.PieChart;
-import com.example.luis.customview2.Yin_yang;
 
 
 public class MainActivity extends AppCompatActivity {
